a8330fc64d52872376c3d4ef1dd6e06b655c076f
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-12-13 13:43:00 +0530

    added google provider

---
+++ Frontend/issuemanager/pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+// [...nextauth].js  is saved in such a way to catch all the routes
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github"
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
--- Frontend/issuemanager/pages/index.tsx
+++ Frontend/issuemanager/pages/index.tsx
@@ -1,4 +1,4 @@
-import Login from '../components/login'
+import Login from './Login'
 export default function Home() {
   return (
     <>
